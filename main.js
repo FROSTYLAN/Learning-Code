eb8b22189929ac7036f0606cb9cf2ef7e3276376
FROSTYLAN <castillo089frosty@gmail.com>
2023-04-28 01:17:50 -0500

    avence

--- main.js
+++ main.js
@@ -14,3 +14,57 @@ img.addEventListener("click", () => {
 menu.addEventListener("click", () => {
   options.classList.toggle("hidden");
 });
+
+const courses = document.getElementById("courses");
+const containerCourse = document.createElement("div");
+
+const coursesArr = [
+  {
+    0: "HTML",
+    1: "./assets/HTML.png",
+    2: "Lenguaje de marcado utilizado oara definir la estructura de una aplicación web.",
+  },
+  {
+    0: "CSS",
+    1: "./assets/CSS.png",
+    2: "Lenguaje que define el estilo y la presentación visual de las páginas web escritas en HTML.",
+  },
+  {
+    0: "JavaScript",
+    1: "./assets/JavaScript.png",
+    2: "JavaScript es un lenguaje de programación que agrega interactividad a las páginas web.",
+  },
+  {
+    0: "React",
+    1: "./assets/React.png",
+    2: "Biblioteca de JavaScript utilizada para construir interfaces de usuario interactivas y reutilizables.",
+  },
+  {
+    0: "Angular",
+    1: "./assets/Angular.png",
+    2: "Framework de JavaScript utilizado para construir aplicaciones web de una sola página (SPA).",
+  },
+  {
+    0: "Vue",
+    1: "./assets/Vue.png",
+    2: "Framework de JavaScript utilizado para construir interfaces de usuario reactivas y progresivas.",
+  },
+];
+
+for (let i = 0; i < coursesArr.length; i++) {
+  const card = document.createElement("div");
+  card.innerHTML = `
+  <div class="card">
+    <div class="card-header">
+      <h2>${coursesArr[i][0]}</h2>
+      <img src="${coursesArr[i][1]}" alt="Icono de la tarjeta" />
+    </div>
+    <div class="card-body">
+      <p>${coursesArr[i][2]}</p>
+      <a href="#">Leer más</a>
+    </div>
+  </div>
+`;
+  containerCourse.appendChild(card);
+}
+courses.appendChild(containerCourse);
--- sphereTech.js
+++ sphereTech.js
@@ -1,5 +1,7 @@
+import * as THREE from "three";
+
 const canvas = document.getElementById("canvas");
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer();
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -8,6 +10,7 @@ const camera = new THREE.PerspectiveCamera(
   1,
   1000
 );
+
 camera.position.z = 5;
 
 const sphereGeometry = new THREE.SphereGeometry(2, 32, 32);
@@ -15,3 +18,49 @@ const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
 scene.add(sphere);
+
+const technologies = [
+  "React",
+  "Vue",
+  "Angular",
+  "Ember",
+  "jQuery",
+  "Bootstrap",
+  "Tailwind CSS",
+  "Sass",
+  "Webpack",
+];
+
+const objectGroup = new THREE.Group();
+
+for (let i = 0; i < technologies.length; i++) {
+  const technology = technologies[i];
+
+  const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+
+  const phi = Math.acos(-1 + (2 * i) / technologies.length);
+  const theta = Math.sqrt(technologies.length * Math.PI) * phi;
+
+  cube.position.x = 2 * Math.cos(theta) * Math.sin(phi);
+  cube.position.y = 2 * Math.sin(theta) * Math.sin(phi);
+  cube.position.z = 2 * Math.cos(phi);
+
+  cube.lookAt(sphere.position);
+
+  objectGroup.add(cube);
+}
+
+scene.add(objectGroup);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
